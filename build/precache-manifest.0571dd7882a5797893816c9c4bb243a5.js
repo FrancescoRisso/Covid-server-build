ff422a5bb72053f6212ce5eac8c6ffdc76a259fc
FrancescoRisso <francesco@frisso.net>
2020-10-31 22:08:17 +0100

    Added the menu functionality

---
+++ build/precache-manifest.0571dd7882a5797893816c9c4bb243a5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27ed0c74f8f55bf59b0e70baa43d76a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8271740ff85212ead1f0",
+    "url": "/static/css/main.5fb8ee3f.chunk.css"
+  },
+  {
+    "revision": "c18eaca67c989a1682d0",
+    "url": "/static/js/2.bdb86226.chunk.js"
+  },
+  {
+    "revision": "ed1ab335b8e242ae7ed4bef7801b3f35",
+    "url": "/static/js/2.bdb86226.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8271740ff85212ead1f0",
+    "url": "/static/js/main.a3ddc9ec.chunk.js"
+  },
+  {
+    "revision": "880ead2686ec4d3085c1",
+    "url": "/static/js/runtime-main.b40e28e2.js"
+  },
+  {
+    "revision": "629cbd8d947de94f359d3194d203080f",
+    "url": "/static/media/invisible.629cbd8d.svg"
+  },
+  {
+    "revision": "af0d8e87ef6acd6655af1ec584089abc",
+    "url": "/static/media/onlyThis.af0d8e87.svg"
+  },
+  {
+    "revision": "320fe8b07fbbdecaec80bd77dfe06fbb",
+    "url": "/static/media/visible.320fe8b0.svg"
+  }
+]);
