dc782ac07600d81ea85ab3afdfe1e8591e7b48f2
FrancescoRisso <francesco@frisso.net>
2020-11-09 10:20:05 +0100

    Trying to debug the server not working properly

---
+++ build/precache-manifest.6522b50f8456e2a590186010ef90c0d9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40a67dbd6fe3d8f7cb37db61d7dbf9ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a08ba14ebcc091176688",
+    "url": "/static/css/main.21dc8b8c.chunk.css"
+  },
+  {
+    "revision": "241b302dea6a6be10bf5",
+    "url": "/static/js/2.7da86d7f.chunk.js"
+  },
+  {
+    "revision": "ed1ab335b8e242ae7ed4bef7801b3f35",
+    "url": "/static/js/2.7da86d7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a08ba14ebcc091176688",
+    "url": "/static/js/main.86992d31.chunk.js"
+  },
+  {
+    "revision": "880ead2686ec4d3085c1",
+    "url": "/static/js/runtime-main.b40e28e2.js"
+  },
+  {
+    "revision": "629cbd8d947de94f359d3194d203080f",
+    "url": "/static/media/invisible.629cbd8d.svg"
+  },
+  {
+    "revision": "af0d8e87ef6acd6655af1ec584089abc",
+    "url": "/static/media/onlyThis.af0d8e87.svg"
+  },
+  {
+    "revision": "320fe8b07fbbdecaec80bd77dfe06fbb",
+    "url": "/static/media/visible.320fe8b0.svg"
+  }
+]);
