02d2beb6c2e810da4cd50eac77dcce6db0b9d3a5
FrancescoRisso <francesco@frisso.net>
2020-12-27 22:16:13 +0100

    Fixing wrong commit

---
+++ build/precache-manifest.fe6e3f0b93f16dc84ad792ed817525ad.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87c353a8a9bc2ca9eb3ba8926772652f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb6828c671709a4bae9c",
+    "url": "/static/css/main.860e1a73.chunk.css"
+  },
+  {
+    "revision": "0f55878efea934b6d06d",
+    "url": "/static/js/2.bd40d71b.chunk.js"
+  },
+  {
+    "revision": "7abd364fd4e4d3f957e0f4028eb8f02e",
+    "url": "/static/js/2.bd40d71b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb6828c671709a4bae9c",
+    "url": "/static/js/main.9cda682c.chunk.js"
+  },
+  {
+    "revision": "880ead2686ec4d3085c1",
+    "url": "/static/js/runtime-main.b40e28e2.js"
+  },
+  {
+    "revision": "1fae162172620c139cc83689d920206c",
+    "url": "/static/media/Italy.1fae1621.svg"
+  },
+  {
+    "revision": "b8833754819db81cf1234b110a0c14d9",
+    "url": "/static/media/blank.b8833754.svg"
+  },
+  {
+    "revision": "f5c0f4574b5110a97b23a0330a698677",
+    "url": "/static/media/graphs2.f5c0f457.svg"
+  },
+  {
+    "revision": "2a5e3fb31ba237b5b89d4f1765b39d9c",
+    "url": "/static/media/home.2a5e3fb3.svg"
+  },
+  {
+    "revision": "b939b939dfd7d4054fefd02233026db1",
+    "url": "/static/media/i.b939b939.svg"
+  },
+  {
+    "revision": "629cbd8d947de94f359d3194d203080f",
+    "url": "/static/media/invisible.629cbd8d.svg"
+  },
+  {
+    "revision": "af0d8e87ef6acd6655af1ec584089abc",
+    "url": "/static/media/onlyThis.af0d8e87.svg"
+  },
+  {
+    "revision": "056267ac9ab2c0c8eaa4bc3b93636978",
+    "url": "/static/media/table1.056267ac.svg"
+  },
+  {
+    "revision": "320fe8b07fbbdecaec80bd77dfe06fbb",
+    "url": "/static/media/visible.320fe8b0.svg"
+  }
+]);
